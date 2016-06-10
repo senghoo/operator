4bf4e4b81133385f51458528fdf918dbbb3ca6c5
Senghoo Kim <shkdmb@gmail.com>
2016-06-10 22:20:27 +0800

    add static path

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import tornado.ioloop
 import tornado.web
 
 import controllers
+import utils
 
 
 def make_app():
     return tornado.web.Application([
         (r"/", controllers.MainHandler),
+        (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": utils.make_path("static")}),
     ], debug=True)
 
 if __name__ == "__main__":
