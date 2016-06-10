ae99ce1fb53371221befff37ce349530eddc2171
Senghoo Kim <shkdmb@gmail.com>
2016-06-10 22:34:03 +0800

    options

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import tornado.ioloop
 import tornado.web
+from tornado.options import define, options
 
 import controllers
+
+define("host", default=8888, type=int)
 
 
 def make_app():
     return tornado.web.Application(controllers.routes, debug=True)
 
 if __name__ == "__main__":
+    options.parse_command_line()
     app = make_app()
-    app.listen(8888)
+    app.listen(options.host)
     tornado.ioloop.IOLoop.current().start()
