1872d533c16be60185fc8c7687a63a3181f309a5
Senghoo Kim <shkdmb@gmail.com>
2016-06-10 22:27:05 +0800

    route

--- controllers.py
+++ controllers.py
@@ -1,8 +1,22 @@
 import tornado.web
 
-from utils import T
+from utils import T, make_path
+
+routes = [
+    (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": make_path("static")}),
+]
 
 
+class R:
+    def __init__(self, route):
+        self.route = route
+
+    def __call__(self, cls):
+        routes.append((self.route, cls))
+        return cls
+
+
+@R("/")
 class MainHandler(tornado.web.RequestHandler):
     @T("index.html")
     def get(self):
